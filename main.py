8af47c8d16a497b16c0b198ff393c9f681d19a08
Daeyeol Sung <tjdeoduf1228@gmail.com>
2025-06-10 11:04:44 +0900

    CORS 설정 추가

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:3000"],
+                   allow_origins=["https://jeonse-guard-frontend.vercel.app",
+                                  "http://localhost:3000"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
